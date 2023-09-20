bcdcb049651c3d5d0a9dd2a6ddd69d209bb7e52c
dinesh02A <145586147+dinesh02A@users.noreply.github.com>
2023-09-20 20:51:49 +0530

    Add files via upload

---
+++ challenge_ unit1/main (1).py
@@ -0,0 +1,5 @@
+YEAR=int(input("Enter a year:"))
+if (YEAR%4==0):
+  print("The given year is leap year")
+else:
+  print("The given year is not a leap year")
---
+++ challenge_ unit1/main.py
@@ -0,0 +1,11 @@
+def fact_rec (n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+
+num=int(input("Ënter a value:"))
+res=fact_rec(num)
+
+print("The factorial of {} is {}.".format(num,res))
+
